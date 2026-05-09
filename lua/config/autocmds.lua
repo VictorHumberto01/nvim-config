9e0f2c7e208dfa29a4bdf2f80a3321774712cd8d
Victor  Gonçalves <vbhy91zr@duck.com>
2026-05-08 23:37:51 -0300

    feat: setup transparency and sharp dashboard banner

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,4 +5,33 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+vim.opt_local.relativenumber = false
+
+-- Transparent background
+local function transparent()
+  local hl_groups = {
+    "Normal",
+    "NormalNC",
+    "NormalFloat",
+    "FloatBorder",
+    "SignColumn",
+    "LineNr",
+    "CursorLineNr",
+    "FoldColumn",
+    "WinSeparator",
+    "EndOfBuffer",
+    "MsgArea",
+  }
+  for _, group in ipairs(hl_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+  end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = transparent,
+})
+
+transparent()
+
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")

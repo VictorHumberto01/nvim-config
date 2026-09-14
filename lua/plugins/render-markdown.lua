d377553e4cbab2bbcf4f97de33d721bfa7736f34
Victor  Gonçalves <vbhy91zr@duck.com>
2026-09-14 10:43:55 -0300

    Add Markdown preview keymap and fix mini.icons dependency

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,7 +1,15 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
+  keys = {
+    {
+      "<leader>mp",
+      "<cmd>RenderMarkdown preview<cr>",
+      ft = { "markdown" },
+      desc = "Markdown Preview Split (dentro do Vim)",
+    },
+  },
 }
